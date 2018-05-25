98281b41f21f29dcb2a0bb0c5e3f145c2595366c
kayleabox <kayleabox09@gmail.com>
2018-05-25 08:50:03 -0500

    [IntroCSPy-week4] - wrapped each function in Word class in tests, need to take another look at it to make sure I cover all of the appropriate cases

--- week4/scrabble/Word.py
+++ week4/scrabble/Word.py
@@ -13,13 +13,9 @@ WORD_LIST = load_words()
 class Word():
   def __init__(self, word = ''):
     self.word = word
-    self.score = 0
-  
+
   def get(self):
     return self.word
-
-  def get_score(self):
-    return self.score
 
   def calculate_score(self, hand_size):
     return self.add_bonus(self.calculate_base_score(), hand_size)
@@ -34,11 +30,11 @@ class Word():
 
   def is_valid(self, hand):
     if self.word != '':
-      if self.check_word_in_hand(hand) != False:
+      if self.check_hand(hand) != False:
         return self.word in WORD_LIST
     return False
 
-  def check_word_in_hand(self, hand):
+  def check_hand(self, hand):
     for letter in self.word:
       if letter not in hand or hand[letter] < self.word.count(letter): 
         return False
--- week4/scrabble/tests/test_word.py
+++ week4/scrabble/tests/test_word.py
@@ -6,11 +6,12 @@ sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.par
 
 from Word import Word
 
-# Word.get()
 class WordTest(unittest.TestCase):
   def test(self):
     self.assertEqual(True, True)
 
+
+# Word.get()
 class WordGetTest1(unittest.TestCase):
   def test(self):
     word = Word('name')
@@ -25,6 +26,44 @@ class WordGetTest3(unittest.TestCase):
   def test(self):
     word = Word('excellent')
     self.assertEqual(word.get(), 'excellent')
+
+
+# Word.check_hand()
+class CheckHandTest1(unittest.TestCase):
+  def test(self):
+    word = Word('yes')
+    hand = {'y': 2, 'c': 1, 'e': 2, 's': 3}
+    self.assertEqual(word.check_hand(hand), None)
+
+class CheckHandTest2(unittest.TestCase):
+  def test(self):
+    word = Word('soup')
+    hand = {'y': 2, 'c': 1, 'e': 2, 's': 3}
+    self.assertEqual(word.check_hand(hand), False)
+
+class CheckHandTest3(unittest.TestCase):
+  def test(self):
+    hand = {'r': 1, 'a': 3, 'p': 2, 't': 1, 'u':2}
+    word = Word('honey')
+    self.assertEqual(word.check_hand(hand), False)
+
+class CheckHandTest4(unittest.TestCase):
+  def test(self):
+    hand = {'e':1, 'v':2, 'n':1, 'i':1, 'l':2}
+    word = Word('evil')
+    self.assertEqual(word.check_hand(hand), None)
+
+class CheckHandTest5(unittest.TestCase):
+  def test(self):
+    hand = {'e':1, 'v':2, 'n':1, 'i':1, 'l':2}
+    word = Word('4547')
+    self.assertEqual(word.check_hand(hand), False)
+
+class CheckHandTest6(unittest.TestCase):
+  def test(self):
+    hand = {'e':1, 'v':2, 'n':1, 'i':1, 'l':2}
+    word = Word('evvni')
+    self.assertEqual(word.check_hand(hand), None)
 
 # word.is_valid
 class IsValidWordTest1(unittest.TestCase):
@@ -69,7 +108,6 @@ class IsValidWordTest7(unittest.TestCase):
     word = Word('3435')
     self.assertEqual(word.is_valid(hand), False)
 
-# Word.get_score()
 
 # Word.calculate_score()
 class WordGetScore1(unittest.TestCase):
@@ -97,7 +135,46 @@ class WordGetScore5(unittest.TestCase):
     word = Word('zebra')
     self.assertEqual(word.calculate_score(4), 80)
 
+
+# Word.calculate_base_score()
 class WordCalculateBaseScore1(unittest.TestCase):
   def test(self):
     word = Word('zebra')
     self.assertEqual(word.calculate_base_score(), 80)
+
+class WordCalculateBaseScore2(unittest.TestCase):
+  def test(self):
+    word = Word('hello')
+    self.assertEqual(word.calculate_base_score(), 40)
+
+class WordCalculateBaseScore3(unittest.TestCase):
+  def test(self):
+    word = Word('')
+    self.assertEqual(word.calculate_base_score(), 0)
+
+
+# Word.add_bonus()
+class WordAddBonus1(unittest.TestCase):
+  def test(self):
+    word = Word('hello')
+    base_score = word.calculate_base_score()
+    self.assertEqual(word.add_bonus(base_score, 7),  40)
+
+class WordAddBonus2(unittest.TestCase):
+  def test(self):
+    word = Word('hello')
+    base_score = word.calculate_base_score()
+    self.assertEqual(word.add_bonus(base_score, 5),  90)
+
+class WordAddBonus3(unittest.TestCase):
+  def test(self):
+    word = Word('stuff')
+    base_score = word.calculate_base_score()
+    self.assertEqual(word.add_bonus(base_score, 7),  55)
+
+class WordAddBonus4(unittest.TestCase):
+  def test(self):
+    word = Word('stuff')
+    base_score = word.calculate_base_score()
+    self.assertEqual(word.add_bonus(base_score, 5),  105)
+
